04b8ce264a7dfca4ae2142d29c4dcf37e9491cb0
Gabriel <gabrielpsouza89@gmail.com>
2025-08-27 11:54:56 +0000

    att entrega

--- aula_3/entrega3.c
+++ aula_3/entrega3.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
 
 int minutos (int hora, int min) {
-    int total;
+    int total = 0;
     total += hora * 60;
     total += min;
 
     return total;
 }
 
-diferen
+diferenca (int total1, int total2) {
+    int diferenca;
+    diferenca = total1 - total2;
+    return diferenca;
+}
 
 int main () {
     int total1, total2, hora, min;
+    int dif;
 
     printf("Digite a primeira hora: ");
-    scanf("%d:%d", hora, min);
-    total1 = 
+    scanf("%d:%d", &hora, &min);
+    total1 = minutos(hora, min);
     printf("Digite a segunda hora: ");
-    scanf("%d:%d", hora, min);
+    scanf("%d:%d", &hora, &min);
+    total2 = minutos(hora, min);
+
+    dif = diferenca(total1, total2);
+
+    printf("As horas deram %d minutos e a segunda deu %d uma diferença de %d em minutos\n", total1, total2, dif);
+    
 }
