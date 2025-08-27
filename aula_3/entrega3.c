d21bc72c5963e1411b5d8430f65f951ff32f0b63
Gabriel <gabrielpsouza89@gmail.com>
2025-08-27 11:50:20 +0000

    entrega

---
+++ aula_3/entrega3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int minutos (int hora, int min) {
+    int total;
+    total += hora * 60;
+    total += min;
+
+    return total;
+}
+
+diferen
+
+int main () {
+    int total1, total2, hora, min;
+
+    printf("Digite a primeira hora: ");
+    scanf("%d:%d", hora, min);
+    total1 = 
+    printf("Digite a segunda hora: ");
+    scanf("%d:%d", hora, min);
+}
